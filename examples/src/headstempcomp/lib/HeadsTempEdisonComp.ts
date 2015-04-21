a2c83c7b1bfacbc180320feae48376a437d6d67e
Olivier Barais <barais@irisa.fr>
2015-04-21 14:24:37 +0200

    new version with update

--- examples/src/headstempcomp/lib/HeadsTempEdisonComp.ts
+++ examples/src/headstempcomp/lib/HeadsTempEdisonComp.ts
@@ -6,17 +6,17 @@ class HeadsTempEdisonComp extends KevoreeEntities.AbstractComponent {
   //dic_msg: KevoreeEntities.DictionaryAttribute = {};
   dic_test: boolean = false;
   dic_period: number = 500;
+  analogPin0: mraa.Aio;
   start(done: (e?: Error) => void): void {
 
     this.log.info(this.toString(), 'start');
-    this.dic_test = this.dictionary.getString('test') === "true";
-
+    this.dic_test = this.dictionary.getBoolean('test');
+    this.dic_period = this.dictionary.getNumber('period')
     if (!this.dic_test) {
-
       //var mraa = require('mraa'); //require mraa
       this.log.info(this.toString(), 'MRAA Version: ' + mraa.getVersion());
-      var analogPin0 = new mraa.Aio(0);
-      this.timerToken = setInterval(() => { var value = analogPin0.read(); this.log.info(this.toString(), ""+ value); this.out_out(""+ value) }, this.dic_period);
+      this.analogPin0 = new mraa.Aio(0);
+      this.timerToken = setInterval(() => { var value = this.analogPin0.read(); this.log.info(this.toString(), "" + value); this.out_out("" + value) }, this.dic_period);
 
     } else {
       this.timerToken = setInterval(() => this.log.info(this.toString(), 'toto'), this.dic_period);
@@ -27,6 +27,18 @@ class HeadsTempEdisonComp extends KevoreeEntities.AbstractComponent {
   stop(done: (err?: Error) => void): void {
     this.log.info(this.toString(), 'stop');
     clearInterval(this.timerToken);
+    done();
+  }
+
+  update(done: (err?: Error) => void): void {
+    this.log.info(this.toString(), 'update');
+    clearInterval(this.timerToken);
+    this.dic_period = this.dictionary.getNumber('period')
+    if (!this.dic_test) {
+      this.timerToken = setInterval(() => { var value = this.analogPin0.read(); this.log.info(this.toString(), "" + value); this.out_out("" + value) }, this.dic_period);
+    } else {
+      this.timerToken = setInterval(() => this.log.info(this.toString(), 'toto'), this.dic_period);
+    }
     done();
   }
 
