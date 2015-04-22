2c1624d5465c6c89706767b76777498b72c7d2d8
Olivier Barais <barais@irisa.fr>
2015-04-22 11:22:03 +0200

    Improve from Maxime's comments

--- examples/src/headstempcomp/lib/HeadsTempEdisonComp.ts
+++ examples/src/headstempcomp/lib/HeadsTempEdisonComp.ts
@@ -4,22 +4,23 @@ import mraa = require('mraa')
 class HeadsTempEdisonComp extends KevoreeEntities.AbstractComponent {
   timerToken: any;
   //dic_msg: KevoreeEntities.DictionaryAttribute = {};
-  dic_test: boolean = false;
-  dic_period: number = 500;
+  dic_test:  KevoreeEntities.DictionaryAttribute = {datatype : KevoreeEntities.DataType.BOOLEAN} ;
+  dic_period:  KevoreeEntities.DictionaryAttribute = {datatype : KevoreeEntities.DataType.INT,defaultValue:500} ;
+
   analogPin0: mraa.Aio;
   start(done: (e?: Error) => void): void {
 
     this.log.info(this.toString(), 'start');
-    this.dic_test = this.dictionary.getBoolean('test');
-    this.dic_period = this.dictionary.getNumber('period')
-    if (!this.dic_test) {
+    var test = this.dictionary.getBoolean('test',true);
+    var period = this.dictionary.getNumber('period',500);
+    if (!test) {
       //var mraa = require('mraa'); //require mraa
       this.log.info(this.toString(), 'MRAA Version: ' + mraa.getVersion());
       this.analogPin0 = new mraa.Aio(0);
-      this.timerToken = setInterval(() => { var value = this.analogPin0.read(); this.log.info(this.toString(), "" + value); this.out_out("" + value) }, this.dic_period);
+      this.timerToken = setInterval(() => { var value = this.analogPin0.read(); this.log.info(this.toString(), "" + value); this.out_out("" + value) }, period);
 
     } else {
-      this.timerToken = setInterval(() => this.log.info(this.toString(), 'toto'), this.dic_period);
+      this.timerToken = setInterval(() => this.log.info(this.toString(), 'toto'), period);
     }
     done();
   }
@@ -33,11 +34,12 @@ class HeadsTempEdisonComp extends KevoreeEntities.AbstractComponent {
   update(done: (err?: Error) => void): void {
     this.log.info(this.toString(), 'update');
     clearInterval(this.timerToken);
-    this.dic_period = this.dictionary.getNumber('period')
-    if (!this.dic_test) {
-      this.timerToken = setInterval(() => { var value = this.analogPin0.read(); this.log.info(this.toString(), "" + value); this.out_out("" + value) }, this.dic_period);
+    var test = this.dictionary.getBoolean('test',true);
+    var period = this.dictionary.getNumber('period')
+    if (!test) {
+      this.timerToken = setInterval(() => { var value = this.analogPin0.read(); this.log.info(this.toString(), "" + value); this.out_out("" + value) }, period);
     } else {
-      this.timerToken = setInterval(() => this.log.info(this.toString(), 'toto'), this.dic_period);
+      this.timerToken = setInterval(() => this.log.info(this.toString(), 'toto'), period);
     }
     done();
   }
@@ -45,7 +47,7 @@ class HeadsTempEdisonComp extends KevoreeEntities.AbstractComponent {
   in_getValue(msg: string): void {
     this.log.info(this.toString(), 'incoming msg: ' + msg);
     var value = this.analogPin0.read();
-    this.out_out("" + value) 
+    this.out_out("" + value)
   }
 
   out_out(msg: string): void { }
